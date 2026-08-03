27751e041a9d4f544bee9a09f518cb46b20dc140
EL-CONS <gabrielcons117@live.com>
2026-08-02 20:58:50 -0700

    backup commit

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 document.getElementById('radio0').checked = true;
 
     const cardPool = [
-        { img: "var(--img-cons)", tier: "tier-5" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: ""},
-        { img: "var(--img-chofis)", tier: "tier-5" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: ""},
-        { img: "var(--img-carlos)", tier: "tier-5" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: ""},
-        { img: "var(--img-coquin)", tier: "tier-5" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: ""},
-        { img: "var(--img-cesar)", tier: "tier-5" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: ""},
-        { img: "var(--img-horacio)", tier: "tier-5" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: ""},
-        { img: "var(--img-borange)", tier: "tier-5" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: ""},
-        { img: "var(--img-borange)", tier: "tier-5" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: ""},
-        { img: "var(--img-joan)", tier: "tier-5" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: ""},
-        { img: "var(--img-owen)", tier: "tier-5" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: ""},
+        { img: "var(--img-cons)", tier: "tier-5" ,desc:"",hp: 80, atk: 60, def: 99, spd: 60, ability: "DOPA-MINE: each cup of coffee consumed increases speed by 100pts, while lowering hp by 10pts"},
+        { img: "var(--img-chofis)", tier: "tier-5" ,desc:"",hp: 100, atk: 100, def: 100, spd: 100, ability: "SAFEGUARD: gains a 75pts shield every turn"},
+        { img: "var(--img-carlos)", tier: "tier-5" ,desc:"",hp: 150, atk: 100, def: 100, spd: 100, ability: "AUDIOPHILE: Listening to apex twin wile playing this cards doubles its defense."},
+        { img: "var(--img-coquin)", tier: "tier-5" ,desc:"Cómo te caeria una coquita zero?",hp: 100, atk: 50, def: 120, spd: 60, ability: "PRO-CASTINATOR: gains 40pts of attack every turn. "},
+        { img: "var(--img-cesar)", tier: "tier-5" ,desc:"",hp:70, atk: 100, def: 100, spd: 100, ability: ""},
+        { img: "var(--img-horacio)", tier: "tier-5" ,desc:"",hp: 65, atk: 100, def: 100, spd: 100, ability: "THE CHOSSEN ONE: when dying, this card revives with 100 pts on each stat."},
+        { img: "var(--img-borange)", tier: "tier-5" ,desc:"inside joke ",hp: 300, atk: 500, def: 0, spd: 10, ability: "CARRIAGE: chose another 5-star card and increase its speed and defense by 100pts, this effect doubles if COQUIN is chossen."},
+        { img: "var(--img-joan)", tier: "tier-5" ,desc:" ",hp: 300, atk: 50, def: 100, spd: 100, ability: "ABARE: if this cards is atacked, it return 50% of damage taken."},
+        { img: "var(--img-owen)", tier: "tier-5" ,desc:"saquenme de guaymas",hp: 100, atk: 100, def: 100, spd: 100, ability: "DENIAL OF SERVICE: every turn a random card in the oponents deck becomes unplayable for the rest of the game."},
         
         
-        { img: "var(--img-error-de-tipo)", tier: "tier-4" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: "Type Coercion: Converts enemy defense to its current hp."},
-        { img: "var(--img-inyeccion-sql)", tier: "tier-4" ,desc:"Always sanitize your database inputs.",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: "Bypass: Ignores enemy shields and attacks directly."},
-        { img: "var(--img-out-of-bounds)", tier: "tier-4" ,desc:"description text",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: "Buffer Overflow: Overwrites adjacent memory, corrupting enemy stats randomly."},
-        { img: "var(--img-sytanx-error)", tier: "tier-4" ,desc:"You forgot the semicolon.",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: "Halt: Skips the enemy's next turn."},
+        { img: "var(--img-error-de-tipo)", tier: "tier-4" ,desc:" == != === ",hp: 100, atk: 100, def: 100, spd: 100, ability: "Type Coercion: Converts enemy defense to its current hp."},
+        { img: "var(--img-inyeccion-sql)", tier: "tier-4" ,desc:"Always sanitize your database inputs.",hp: 100, atk: 100, def: 100, spd: 100, ability: "Bypass: Ignores enemy shields and attacks directly."},
+        { img: "var(--img-out-of-bounds)", tier: "tier-4" ,desc:"the thing you are looking for is not here",hp: 100, atk: 100, def: 100, spd: 100, ability: "Buffer Overflow: Overwrites adjacent memory, corrupting enemy stats randomly."},
+        { img: "var(--img-sytanx-error)", tier: "tier-4" ,desc:"You forgot the semicolon.",hp: 100, atk: 100, def: 100, spd: 100, ability: "Halt: Skips the enemy's next turn."},
 
-        { img: "var(--img-cplusplus)", tier: "tier-3",desc:"Makes it harder to shoot yourself in the foot, but when you do it blows your whole leg off." ,hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: "Manual Memory: High risk, high reward damage."},
-        { img: "var(--img-java)", tier: "tier-3" ,desc:"Write once, debug anywhere.",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: "Garbage Collector: Recovers 10% HP for 3 turns."},
-        { img: "var(--img-python)", tier: "tier-3" ,desc:"Executable pseudocode.",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: "Import Module: Adapts to any element."},
-        { img: "var(--img-bash)", tier: "tier-3" ,desc:"To err is human, to really foul things up requires root privileges.",hp: 9999, atk: 9999, def: 9999, spd: 9999, ability: "Pipe: Redirects an attack back to them."}
+        { img: "var(--img-cplusplus)", tier: "tier-3",desc:"Makes it harder to shoot yourself in the foot, but when you do it blows your whole leg off." ,hp: 100, atk: 100, def: 100, spd: 100, ability: "Manual Memory: High risk, high reward damage."},
+        { img: "var(--img-java)", tier: "tier-3" ,desc:"Write once, debug anywhere.",hp: 100, atk: 100, def: 100, spd: 100, ability: "Garbage Collector: Recovers 10% HP for 3 turns."},
+        { img: "var(--img-python)", tier: "tier-3" ,desc:"Executable pseudocode.",hp: 100, atk: 100, def: 100, spd: 100, ability: "Import Module: Adapts to any element."},
+        { img: "var(--img-bash)", tier: "tier-3" ,desc:"To err is human, to really foul things up requires root privileges.",hp: 100, atk: 100, def: 100, spd: 100, ability: "Pipe: Redirects an attack back to them."}
         /* 
         
         // --- LENGUAJES ---
@@ -83,7 +82,7 @@ document.getElementById('radio0').checked = true;
             miniCardElement.innerHTML = `
                 <div class="card-stats-side">
                     <span>HP: ${cardData.hp} ATK: ${cardData.atk}</span>
-                    <span>DEF: ${cardData.def}</span>
+                    <span>DEF: ${cardData.def} SPD: ${cardData.spd}</span>
                 </div>
                 <div class="card-desc-bottom">
                     <div class="ability-text">${cardData.ability}</div>
